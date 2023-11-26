1157567fc66a2579de5c6bb43fa1b14cfdd9aa14
hinoha <57580824+hinoha@users.noreply.github.com>
2023-11-26 20:29:38 +0800

    Update Credits.tsx

--- src/components/credits/Credits.tsx
+++ src/components/credits/Credits.tsx
@@ -6,7 +6,7 @@ const Credits = () => {
       <div className={creditTitleStyles}>how & who</div>
       <div className={contStyles}>
         <b>How to play:</b> <br />
-        Progress from level to level. Become more than strangers, one card at a time.
+        Get to know each other, one card at a time.
         <p>Refresh to reset card decks.</p>{" "}
         <p>
           Made by <a href="https://instagram.com/jdyjuanco">@jdyjuanco</a>.
